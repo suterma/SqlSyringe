cf4e6dd88c83fa62772fd1e87254a95f3859ce78
Marcel <suterma@users.noreply.github.com>
2019-10-17 08:50:21 +0200

    Skeleton created

---
+++ SqlSyringe.Patient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SqlSyringe.Patient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

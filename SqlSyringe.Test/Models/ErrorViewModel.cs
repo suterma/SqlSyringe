a318a996b8ce336e286e94e69ed7b93ce494e8cf
Marcel <suterma@users.noreply.github.com>
2019-10-17 17:02:01 +0200

    Updated dependency, removed demo project

---
+++ SqlSyringe.Test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SqlSyringe.Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
